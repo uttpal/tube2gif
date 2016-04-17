9d07d1ee7b2c0b326f093326e84af2988f3d1111
Kumar Uttpal <uttpal25993@gmail.com>
2016-04-17 18:07:47 +0530

    minor fixes

--- controller/gif.js
+++ controller/gif.js
@@ -4,21 +4,20 @@ var request = require("request");
 module.exports = function getGif(req, res, next) {
 
   var id = encodeURIComponent(req.body.id);
-  var start = req.body.start * 60;
-  var end = start + parseInt(req.body.duration);
-  console.log(start,end, parseInt(req.body.duration));
+  var start = (req.body.start * 60);
+  var end = (start + parseInt(req.body.duration)).toString();
 
-  var options = {
-    method: 'GET',
-    url: 'http://apilayer.net/api/capture',
-    qs: {
-      access_key: '2ed1edfe06b18fda5ae7003d7994a840',
-      url: 'https://www.youtube.com/watch?v=' + id,
-      start: start,
-      end: end
-    }
-  };
+  var options = { method: 'GET',
+  url: 'http://apilayer.net/api/capture',
+  qs:
+   { access_key: '1f68677082a6fe0a36f0bef2fba84ec7',
+     url: 'https://www.youtube.com/watch?v=' + id,
+     start: start.toString(),
+     end: end }
+    };
 
+
+    console.log(options);
   request(options).on('response', function(res) {
     delete res.request;
   }).pipe(res);
