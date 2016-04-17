158781d6eeaadd99dce529fabec5be750df74c94
Kumar Uttpal <uttpal25993@gmail.com>
2016-04-17 12:14:58 +0530

    controller: search, template modification

---
+++ controller/search.js
@@ -0,0 +1,25 @@
+'use strict';
+const request = require('request');
+
+module.exports = function search(req, res, next) {
+  const query = encodeURIComponent(req.query.q);
+  var options = {
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    qs: {
+      part: 'snippet',
+      maxResults: '15',
+      order: 'viewCount',
+      q: query,
+      type: 'video',
+      key: ' AIzaSyCHN0IzD3Aj9eMY2b_XHwWJYnLWWUv41ao'
+    }
+  };
+
+  request(options, function(err, response, videosObj) {
+    videosObj = JSON.parse(videosObj);
+    res.render('search', {
+      videos: videosObj
+    });
+  });
+};
