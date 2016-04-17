cefe5393638cbb4b3e11625a57906dd1c776704e
Kumar Uttpal <uttpal25993@gmail.com>
2016-04-17 15:16:15 +0530

    routes: search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const search = require("../controller/search.js")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
+router.get('/search', search);
 module.exports = router;
