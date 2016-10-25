123b9a37cd3567882cd0989f2abb52b4a769368d
Uttpal <uttpal25993@gmail.com>
2016-10-25 17:27:47 +0530

    fix input

--- controller/gif.js
+++ controller/gif.js
@@ -5,18 +5,16 @@ module.exports = (req, res, next) => {
 
   const id = encodeURIComponent(req.body.id);
   const start = new Date(req.body.start * 1000).toISOString().substr(11, 8);
-  const end = (start + parseInt(req.body.duration));
+  const duration = parseInt(req.body.duration);
 
   const video = {
     url: 'https://www.youtube.com/watch?v=' + id,
     start: start,
-    end: end
+    duration: duration
   };
 
-  console.log(video);
   gif.generate(video)
     .then((filename) => {
-      console.log(19,filename);
       res.render('gif', {
         filename: filename
       });
